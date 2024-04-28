4cff12309aa06a6d76a383a31e3cdd4dfbe25394
qq5460168 <109363085+qq5460168@users.noreply.github.com>
2024-04-29 00:43:45 +0800

    Update title.py

--- data/python/title.py
+++ data/python/title.py
@@ -19,8 +19,8 @@ for file_path in file_list:
     line_count = content.count('\n') + 1
 
     # 在文件顶部插入内容
-    new_content = f"[Adblock Plus 2.0]\n" \
-                  f"去广告规则，反馈🐧群930869948\n" \
+    new_content = f"[个人合并 2.0]\n" \
+                  f"! Title: 去广告规则，反馈🐧群930869948\n" \
                   f"! Homepage: https://github.com/qq5460168/666\n" \
                   f"! Expires: 12 Hours\n" \
                   f"! Version: {beijing_time}（北京时间）\n" \
