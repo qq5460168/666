90f86153d8c7be475fa308f9b489af44559d6295
qq5460168 <109363085+qq5460168@users.noreply.github.com>
2024-06-13 03:05:38 +0800

    更新 update-rules.sh

--- data/script/update-rules.sh
+++ data/script/update-rules.sh
@@ -37,7 +37,7 @@ rules=(
   "https://raw.githubusercontent.com/mphin/AdGuardHomeRules/main/Blacklist.txt" #mphin
   "https://gitee.com/zjqz/ad-guard-home-dns/raw/master/black-list" #周木木
   "https://raw.githubusercontent.com/liwenjie119/adg-rules/master/black.txt" #liwenjie119
-      #"https://github.com/qq5460168/dangchu/blob/main/T%E7%99%BD%E5%90%8D%E5%8D%95.txt" #T白
+      #"https://raw.githubusercontent.com/qq5460168/dangchu/main/T%E7%99%BD%E5%90%8D%E5%8D%95.txt" #T白
       "" #
     
  
