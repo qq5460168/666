ecfe1a2d8b469572847bc66845d6bc6eedfa7c17
qq5460168 <109363085+qq5460168@users.noreply.github.com>
2025-05-09 13:05:13 +0800

    更新 filter-dns.py

--- data/python/filter-dns.py
+++ data/python/filter-dns.py
@@ -1,7 +1,7 @@
 import datetime
 
 # 打开原始文件和目标文件
-with open('.././rules.txt', 'r') as input_file, open('.././dns.txt', 'w') as output_file:
+with open('.././rules.txt', 'r') as input_file, open('.././dns.txt', 'w') as dns_output_file, open('.././hosts.txt', 'w') as hosts_output_file:
     # 逐行读取原始文件内容
     for line in input_file:
         # 去除行尾的换行符
@@ -14,5 +14,9 @@ with open('.././rules.txt', 'r') as input_file, open('.././dns.txt', 'w') as out
         
         # 检查行长度是否大于等于2，并且首字符是"||"并且结尾是"^"
         if len(line) >= 2 and line.startswith("||") and line.endswith("^"):
-            # 将满足条件的行写入目标文件
-            output_file.write(line + '\n')
+            # 写入到 DNS 格式文件
+            dns_output_file.write(line + '\n')
+            
+            # 生成 Hosts 格式规则（0.0.0.0 <域名>）
+            domain = line[2:-1]  # 去掉 "||" 和 "^"
+            hosts_output_file.write(f"0.0.0.0 {domain}\n")
