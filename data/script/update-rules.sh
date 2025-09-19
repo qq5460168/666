d7c2d6e9e6c74e672962dcb3f24997dd30689da8
qq5460168 <109363085+qq5460168@users.noreply.github.com>
2025-09-20 01:23:13 +0800

    更新 update-rules.sh

--- data/script/update-rules.sh
+++ data/script/update-rules.sh
@@ -26,7 +26,7 @@ rules=(
   "https://raw.githubusercontent.com/damengzhu/banad/main/jiekouAD.txt" #大萌主
   "https://raw.githubusercontent.com/afwfv/DD-AD/main/rule/DD-AD.txt"  #DD
   "https://raw.githubusercontent.com/Cats-Team/dns-filter/main/abp.txt" #AdRules DNS Filter
-  "https://raw.hellogithub.com/hosts" #GitHub加速
+ # "https://raw.hellogithub.com/hosts" #GitHub加速
   "https://raw.githubusercontent.com/qq5460168/dangchu/main/adhosts.txt" #测试hosts
   "https://raw.githubusercontent.com/qq5460168/dangchu/main/white.txt" #白名单
   "https://raw.githubusercontent.com/qq5460168/Who520/refs/heads/main/Other%20rules/Replenish.txt"#补充
